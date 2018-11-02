268dba37a831df9d41d347c8abb63f4f5e71395e
mesudak <mesudak@gmail.com>
2018-11-02 16:51:23 +0900

    demo update to use ace editor

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,7 @@
-var gl, cml, ctrl, enemyActor, playerActor, playerGeomBase, playerGeom;
+var cml, ctrl, enemyActor, playerActor, playerGeomBase, playerGeom;
+
 class Control {
-  constructor() {this._ = [];this.enabled=true;}
+  constructor() {this._ = [];this.enabled=true;this.isPlaying=false;}
   onKeyDown(keyCode) { if (this.enabled) this._[keyCode] = true; }
   onKeyUp(keyCode)   { if (this.enabled) this._[keyCode] = false; }
   get dx() {return this._[37]?-1:this._[39]?1:0;}
@@ -9,89 +10,80 @@ class Control {
   get ctrl() {return this._[17];}
   get Z() {return this._[90];}
   get X() {return this._[88];}
-}
-
-function runscript(e) {
-  const seq = cml.sequence(document.getElementById('cmlscript').value);
-  if (enemyActor) enemyActor.runner.destroy(0);
-  enemyActor = new Actor(2, cml.runner(0,0,0, seq));
-  return false;
-}
-
-function stopscript(e) {
-  if (enemyActor) enemyActor.runner.destroy(0);
-  return false;
-}
-
-function playerMove() {
-  const p = playerGeom.getAttribute("position");
-  for (let i=0; i<playerGeomBase.length; i++) {
-    const v = playerGeomBase[i].clone().multiplyScalar(1);
-    p.array[i*3] = v.x;
-    p.array[i*3+1] = v.y;
-    p.array[i*3+2] = v.z;
+  runscript() {
+    if (this.isPlaying) {
+      if (enemyActor) enemyActor.runner.destroy(0);
+    } else {
+      const seq = cml.sequence(editor.getValue());
+      if (enemyActor) enemyActor.runner.destroy(0);
+      enemyActor = new Actor(2, cml.runner(0,0,0, seq));
+    }
+    this.isPlaying = !this.isPlaying;
+    return false;
   }
-  p.needsUpdate = true;
-  playerGeom.computeVertexNormals();
-
-  playerActor.runner.setVelocity(ctrl.dx*5, ctrl.dy*5);
 }
 
 
-window.onload = ()=>{
-  ctrl = new Control();
-  cml = new CannonML();
-  gl = new Ptolemy({width:480, height:480,
-    onInitialize:()=>{
-      const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(480, 480), new THREE.MeshLambertMaterial({color: 0xffffff}));
-      floor.position.z = -10;
-      floor.receiveShadow = true;
-      gl.scene.add(floor);
-    },
-    onUpdate:dt=>{
-      cml.update();
-      playerMove();
+new Ptolemy({
+  width:480, 
+  height:480,
+  containerid:'screen',
+  setup:gl=>{
+    ctrl = new Control();
+    cml = new CannonML();
+
+    const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(480, 480), new THREE.MeshLambertMaterial({color: 0xffffff}));
+    floor.position.z = -10;
+    floor.receiveShadow = true;
+    gl.scene.add(floor);
+
+
+    playerGeom = new THREE.IcosahedronBufferGeometry(20, 1);
+    playerGeom.getAttribute("position").dynamic = true;
+    playerGeom.getAttribute("normal").dynamic = true;
+    const array = playerGeom.getAttribute("position").clone().array;
+    playerGeomBase = [];
+    for (let i=0; i<array.length; i+=3) {
+      playerGeomBase.push(new THREE.Vector3(array[i], array[i+1], array[i+2]));
+    } 
+
+    Actor.scene = gl.scene;
+    Actor.avatars = [
+      {geometory:new THREE.ConeBufferGeometry(10, 20, 4), material:new THREE.MeshLambertMaterial({color: 0x8888cc, flatShading:true})},
+      {geometory:playerGeom, material:new THREE.MeshLambertMaterial({color: 0xcc8888, flatShading:true})},
+      {geometory:new THREE.TorusBufferGeometry(15, 5, 4, 8 ), material:new THREE.MeshLambertMaterial({color: 0xffffff})}
+    ];
+    playerActor = new Actor(1, cml.runner(0, -160, 0));
+    playerActor.runner.scopeEnabled = false;
+    cml.setDefaultTarget(playerActor.runner);
+    cml.setScreenSize(400,400,0);
+
+    editor = ace.edit("editor");
+    editor.setValue("#move{i60~px150i120[~px-150~px150]}py100&move");
+    editor.commands.addCommand({
+      name : "play",
+      bindKey: {win:"Ctrl-Enter", mac:"Command-Enter"},
+      exec: ctrl.runscript.bind(ctrl)
+    });
+    editor.on('blur', ()=>ctrl.enabled=true);
+    editor.on('focus',()=>ctrl.enabled=false);
+    document.getElementById('runscript').addEventListener('click', ctrl.runscript.bind(ctrl));
+    document.body.addEventListener('keydown', e=>ctrl.onKeyDown(e.keyCode));
+    document.body.addEventListener('keyup',   e=>ctrl.onKeyUp(e.keyCode));
+  },
+  draw:gl=>{
+    cml.update();
+    playerActor.runner.setVelocity(ctrl.dx*5, ctrl.dy*5);
+  
+    const p = playerGeom.getAttribute("position");
+    for (let i=0; i<playerGeomBase.length; i++) {
+      const v = playerGeomBase[i].clone().multiplyScalar(1);
+      p.array[i*3] = v.x;
+      p.array[i*3+1] = v.y;
+      p.array[i*3+2] = v.z;
     }
-  });
-
-  playerGeom = new THREE.IcosahedronBufferGeometry(20, 1);
-  playerGeom.getAttribute("position").dynamic = true;
-  playerGeom.getAttribute("normal").dynamic = true;
-
-  Actor.scene = gl.scene;
-  Actor.avatars = [
-    {geometory:new THREE.ConeBufferGeometry(10, 20, 4), material:new THREE.MeshLambertMaterial({color: 0x8888cc, flatShading:true})},
-    {geometory:playerGeom, material:new THREE.MeshLambertMaterial({color: 0xcc8888, flatShading:true})},
-    {geometory:new THREE.TorusBufferGeometry(15, 5, 4, 8 ), material:new THREE.MeshLambertMaterial({color: 0xffffff})}
-  ];
-  const array = playerGeom.getAttribute("position").clone().array;
-  playerGeomBase = [];
-  for (let i=0; i<array.length; i+=3) {
-    playerGeomBase.push(new THREE.Vector3(array[i], array[i+1], array[i+2]));
-  } 
-
-  playerActor = new Actor(1, cml.runner(0, -160, 0));
-  playerActor.runner.scopeEnabled = false;
-  cml.setDefaultTarget(playerActor.runner);
-  cml.setScreenSize(400,400,0);
-
-  document.getElementById('container').appendChild(gl.domElement);
-  document.getElementById('cmlscript').value = "#move{i60~px150i120[~px-150~px150]}py100&move";
-  document.getElementById('runscript').addEventListener('click', runscript);
-  document.getElementById('stopscript').addEventListener('click', stopscript);
-
-  document.getElementById('cmlscript').addEventListener('focus', e=>{ctrl.enabled=false});
-  document.getElementById('cmlscript').addEventListener('focusout', e=>{ctrl.enabled=true});
-
-  document.body.addEventListener('keydown', e=>{
-    if (e.ctrlKey && e.key == 's') {
-      e.preventDefault();
-      return runscript();
-    }
-    ctrl.onKeyDown(e.keyCode);
-  });
-  document.body.addEventListener('keyup', e=>ctrl.onKeyUp(e.keyCode));
-
-  gl.start();
-};
+    p.needsUpdate = true;
+    playerGeom.computeVertexNormals();
+  }
+});
 
--- demo/ptolemy.js
+++ demo/ptolemy.js
@@ -1,11 +1,22 @@
 class Ptolemy {
+  /**
+   * three.js wrapper comfortable for me
+   * @param option.containerid container id to insert three.js screen, start automatically when this option is set
+   * @param option.width screen width, undefined to fit client window
+   * @param option.height screen height, undefined to fit client window
+   * @param option.setup called at first of all
+   * @param option.draw called on each frames
+   * @param option.paused flag to paused start
+   */
   constructor(option) {
     option = option || {};
+
     // member valiables
-    this.paused = false;
-    this.prevtime = 0;
-    this.starttime = 0;
+    this._starttime = 0;
+    this._prevtime = 0;
+    this.paused = Boolean(option.paused);
     this.time = 0;
+    this.deltaTime = 0;
     
     // create basic instance
     this.camera   = new THREE.PerspectiveCamera(30, 1, 0.1, 10000);
@@ -14,60 +25,82 @@ class Ptolemy {
 
     // other
     this.screenCenter = new THREE.Vector3(0,0,0);
+    this.screenSize = {"width":option.width||0, "height":option.height||0};
 
     // start
-    this.setSize(option.width || window.innerWidth, option.height || window.innerHeight);
-    this.onUpdate = option.onUpdate;
-    this.onInitialize = option.onInitialize;
+    this._containerid = option.containerid || null;
+    this.setup = option.setup || null;
+    this.draw  = option.draw  || null;
+
+    // insert screen and start automatically, when option.containerid in set
+    if (this._containerid) {
+      window.addEventListener("load", this.start.bind(this));
+      window.addEventListener('resize', this._adjustScreen.bind(this));
+    }
   }
 
-  get domElement() { return this.renderer.domElement; }
+  get cameraDistance() {
+    return this.renderer.getSize().height * 0.5 / Math.tan(this.camera.fov * 0.008726646259971648);
+  } 
 
-  get cameraDistance() { return this.renderer.getSize().height * 0.5 / Math.tan(this.camera.fov * 0.008726646259971648); } 
+  /** screen size */
+  setSize(width, height) {
+    this.screenSize = {"width":width||0, "height":height||0};
+    this._adjustScreen();
+  }
 
+  /** start updating */
   start() {
-    this.starttime = this.prevtime = performance.now();
-    this.setup();
+    if (this._containerid) document.getElementById(this._containerid).appendChild(this.renderer.domElement);
+    this._starttime = this._prevtime = performance.now();
+    this._setup();
     this._loop();
   }
 
-  _loop() {
-    const now = performance.now();
-    this.time = (now - this.starttime) / 1000;
-    if (!this.paused) this.draw((now - this.prevtime) / 1000);
-    this.prevtime = now;
-    requestAnimationFrame(this._loop.bind(this));
-  }
-
-  setSize(width, height) {
+  _adjustScreen() {
+    const width  = this.screenSize.width  || window.innerWidth,
+          height = this.screenSize.height || window.innerHeight;
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(width, height);
   }
 
-  setup() {
+  _loop() {
+    const now = performance.now();
+    this.time      = (now - this._starttime) / 1000;
+    this.deltaTime = (now - this._prevtime) / 1000;
+    this._prevtime = now;
+    if (!this.paused) this._draw();
+    requestAnimationFrame(this._loop.bind(this));
+  }
+
+  _setup() {
+    this._adjustScreen();
+
     this.renderer.gammaOutput = true;
     this.renderer.physicallyCorrectLights = true;
     this.renderer.shadowMap.enabled = true;
 
-    this.light = new THREE.PointLight( 0xffffff, 0.7, 700);
+    this.ambient = new THREE.AmbientLight( 0xffffff, 0.7 );
+    this.light   = new THREE.PointLight( 0xffffff, 0.7, 700);
     this.light.castShadow = true; 
     this.light.shadow.camera.near = 150;
     this.light.shadow.camera.far = 700;
     this.light.shadow.camera.updateProjectionMatrix();
     this.light.position.set(0, 200, 200);
-    this.scene.add(this.light);
-    this.amb = new THREE.AmbientLight( 0xffffff, 0.7 );
-    this.scene.add(this.amb);
-    this.scene.add(this.camera);
+
     this.camera.position.set(0, 0, this.cameraDistance);
     this.camera.lookAt(this.screenCenter);
 
-    if (this.onInitialize) this.onInitialize();
+    this.scene.add(this.ambient);
+    this.scene.add(this.light);
+    this.scene.add(this.camera);
+
+    if (this.setup) this.setup(this);
   }
 
-  draw(deltaTime) {
-    if (this.onUpdate) this.onUpdate(deltaTime);
+  _draw() {
+    if (this.draw) this.draw(this);
     this.renderer.render(this.scene, this.camera);
   }
 }
